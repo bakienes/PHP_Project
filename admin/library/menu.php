5ef15a7e1ed9accd649f38d174816269f768b342
Baki Enes Yalçın <82122237+bakienes@users.noreply.github.com>
2021-04-23 04:15:36 +0300

    Add files via upload

---
+++ admin/library/menu.php
@@ -0,0 +1,82 @@
+
+          <span class="ti-view-list"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="indeks.php">
+              <i class="ti-shield menu-icon"></i>
+              <span class="menu-title">Anasayfa Ayarları</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">
+              <i class="ti-shield menu-icon"></i>
+              <span class="menu-title">Hakkımızda Ayarları</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="service.php">
+              <i class="ti-shield menu-icon"></i>
+              <span class="menu-title">Hizmetlerimiz Ayarları</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="price.php">
+              <i class="ti-shield menu-icon"></i>
+              <span class="menu-title">Fiyatlarımız Ayarları</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="useradd.php">
+              <i class="ti-shield menu-icon"></i>
+              <span class="menu-title">Yeni Kullanıcı Oluştur</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="memberlist.php">
+              <i class="ti-palette menu-icon"></i>
+              <span class="menu-title">Üye Listesi</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="useredit.php">
+              <i class="ti-layout-list-post menu-icon"></i>
+              <span class="menu-title">Üye Ekle</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="lesson.php">
+              <i class="ti-pie-chart menu-icon"></i>
+              <span class="menu-title">Dersler</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="addlesson.php">
+              <i class="ti-view-list-alt menu-icon"></i>
+              <span class="menu-title">Ders Ekle</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="slider.php">
+              <i class="ti-star menu-icon"></i>
+              <span class="menu-title">Slider Ekle</span>
+            </a>
+          </li>
+		  <li class="nav-item">
+			  <a class="nav-link" href="totaldonate.php">
+				  <i class="ti-shield menu-icon"></i>
+				  <span class="menu-title">Toplam Bağış</span>
+			  </a>
+			</li>
+        </ul>
+      </nav>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+         
